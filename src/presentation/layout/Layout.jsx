80b1d72ca9d36ffb3ab19a0293f49334b63479c0
Carlos Arteaga <carlosluis199924@hotmail.com>
2024-11-14 04:14:25 -0500

    el layout ya sale bien encime de todo

--- src/presentation/layout/Layout.jsx
+++ src/presentation/layout/Layout.jsx
@@ -66,97 +66,97 @@ const Layout = ({ children }) => {
         setOpenMenuIndex(openMenuIndex === index ? null : index);
     };
 
-    return (
-        <div className="min-h-screen flex flex-col bg-[#015198] text-gray-800">
-            {/* Barra superior */}
-            <header className="bg-gray-100 text-gray-800 p-2 flex justify-between items-center shadow-lg z-50">
-                <div className="flex items-center space-x-4">
-                    <button
-                        onClick={toggleSidebar}
-                        className="p-2 rounded-full hover:bg-[#015198] hover:text-white transition-all"
-                    >
-                        {isSidebarVisible ? (
-                            <XMarkIcon className="h-6 w-6" />
-                        ) : (
-                            <Bars3Icon className="h-6 w-6" />
-                        )}
-                    </button>
-                    <img src={logo} alt="Logo de Portocomercio" className="h-14 w-auto" />
-                </div>
-                <div className="flex items-center space-x-4">
-                    <span className="font-semibold text-lg">{nombreUsuario}</span>
-                    <div className="rounded-full w-10 h-10 bg-blue-500 flex justify-center items-center text-gray-800 font-semibold">
-                        {/* mostrar solo al primer letra del nombre */}
-                        {nombreUsuario.slice(0, 1)}
-                    </div>
-                </div>
-            </header>
-
-            {/* Contenedor principal que incluye la barra lateral y el contenido */}
-            <div
-                className={`flex flex-grow overflow-hidden transition-all duration-300 ease-in-out`}
-            >
-                {/* Barra lateral */}
-                <aside
-                    className={`bg-[#015198] text-white p-6 space-y-6 w-64 h-full fixed transition-transform duration-300 ease-in-out ${isSidebarVisible ? 'translate-x-0 opacity-100' : '-translate-x-full opacity-0'
-                        }`}
+    return (<div className="min-h-screen flex flex-col bg-[#015198] text-gray-800">
+        {/* Barra superior */}
+        <header className="bg-gray-100 text-gray-800 p-2 flex justify-between items-center shadow-lg z-50 fixed w-full top-0 left-0">
+            <div className="flex items-center space-x-4">
+                <button
+                    onClick={toggleSidebar}
+                    className="p-2 rounded-full hover:bg-[#015198] hover:text-white transition-all"
                 >
-                    <nav className="space-y-4">
-                        {menuData.map((item, index) => (
-                            <div key={index}>
-                                {/* Opción principal con el ícono dentro del <a> */}
-                                <a
-                                    href={item.href}
-                                    className="flex items-center p-2 hover:bg-blue-600 rounded-md transition-colors flex-1 justify-between"
-                                    onClick={() => toggleSubOptions(index)} // Manejar clic para expandir/contraer
-                                >
-                                    {item.name}
-
-                                    {/* Indicador de subopciones */}
-                                    {item.subOptions.length > 0 && (
-                                        <span className="ml-2">
-                                            {openMenuIndex === index ? (
-                                                <ChevronUpIcon className="h-5 w-5 text-white" />
-                                            ) : (
-                                                <ChevronDownIcon className="h-5 w-5 text-white" />
-                                            )}
-                                        </span>
-                                    )}
-                                </a>
-
-                                {/* Subopciones (solo visibles si 'openMenuIndex' coincide con el índice actual) */}
-                                {item.subOptions.length > 0 && (
-                                    <div
-                                        className={`ml-4 overflow-hidden transition-all duration-300 ease-in-out ${openMenuIndex === index ? 'max-h-[1000px]' : 'max-h-0'
-                                            }`}
-                                    >
-                                        <div className="space-y-2">
-                                            {item.subOptions.map((subItem, subIndex) => (
-                                                <a
-                                                    key={subIndex}
-                                                    href={subItem.href}
-                                                    className="block p-2 hover:bg-blue-500 rounded-md transition-colors"
-                                                >
-                                                    {subItem.name}
-                                                </a>
-                                            ))}
-                                        </div>
-                                    </div>
-                                )}
-                            </div>
-                        ))}
-                    </nav>
-                </aside>
-
-                {/* Contenido principal */}
-                <main
-                    className={`flex-grow  bg-white shadow-inner transition-all duration-300 ease-in-out ${isSidebarVisible ? 'ml-64' : 'ml-0'
-                        }`}
-                >
-                    {children}
-                </main>
+                    {isSidebarVisible ? (
+                        <XMarkIcon className="h-6 w-6" />
+                    ) : (
+                        <Bars3Icon className="h-6 w-6" />
+                    )}
+                </button>
+                <img src={logo} alt="Logo de Portocomercio" className="h-14 w-auto" />
             </div>
+            <div className="flex items-center space-x-4">
+                <span className="font-semibold text-lg">{nombreUsuario}</span>
+                <div className="rounded-full w-10 h-10 bg-blue-500 flex justify-center items-center text-gray-800 font-semibold">
+                    {/* mostrar solo al primer letra del nombre */}
+                    {nombreUsuario.slice(0, 1)}
+                </div>
+            </div>
+        </header>
+
+        {/* Contenedor principal que incluye la barra lateral y el contenido */}
+        <div
+            className={`flex flex-grow overflow-hidden transition-all duration-300 ease-in-out pt-16`}
+        >
+            {/* Barra lateral */}
+            <aside
+                className={`bg-[#015198] text-white p-6 space-y-6 w-64 h-full fixed transition-transform duration-300 ease-in-out ${isSidebarVisible ? 'translate-x-0 opacity-100' : '-translate-x-full opacity-0'
+                    }`}
+            >
+                <nav className="space-y-4">
+                    {menuData.map((item, index) => (
+                        <div key={index}>
+                            {/* Opción principal con el ícono dentro del <a> */}
+                            <a
+                                href={item.href}
+                                className="flex items-center p-2 hover:bg-blue-600 rounded-md transition-colors flex-1 justify-between"
+                                onClick={() => toggleSubOptions(index)} // Manejar clic para expandir/contraer
+                            >
+                                {item.name}
+
+                                {/* Indicador de subopciones */}
+                                {item.subOptions.length > 0 && (
+                                    <span className="ml-2">
+                                        {openMenuIndex === index ? (
+                                            <ChevronUpIcon className="h-5 w-5 text-white" />
+                                        ) : (
+                                            <ChevronDownIcon className="h-5 w-5 text-white" />
+                                        )}
+                                    </span>
+                                )}
+                            </a>
+
+                            {/* Subopciones (solo visibles si 'openMenuIndex' coincide con el índice actual) */}
+                            {item.subOptions.length > 0 && (
+                                <div
+                                    className={`ml-4 overflow-hidden transition-all duration-300 ease-in-out ${openMenuIndex === index ? 'max-h-[1000px]' : 'max-h-0'
+                                        }`}
+                                >
+                                    <div className="space-y-2">
+                                        {item.subOptions.map((subItem, subIndex) => (
+                                            <a
+                                                key={subIndex}
+                                                href={subItem.href}
+                                                className="block p-2 hover:bg-blue-500 rounded-md transition-colors"
+                                            >
+                                                {subItem.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                </nav>
+            </aside>
+
+            {/* Contenido principal */}
+            <main
+                className={`flex-grow bg-white shadow-inner transition-all duration-300 ease-in-out ${isSidebarVisible ? 'ml-64' : 'ml-0'
+                    }`}
+            >
+                {children}
+            </main>
         </div>
+    </div>
+
     );
 };
 
