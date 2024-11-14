82eb64a567a1fc8e5e805b058a233b790cf48531
Carlos Arteaga <carlosluis199924@hotmail.com>
2024-11-14 03:46:56 -0500

    layout mejorado

--- src/presentation/layout/Layout.jsx
+++ src/presentation/layout/Layout.jsx
@@ -1,49 +1,165 @@
-import PropTypes from "prop-types";
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Bars3Icon, XMarkIcon, ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid';
+import logo from '../../assets/logo_bar.webp';
 
+const menuData = [
+    {
+        name: 'Opción 1',
+        href: '#opcion1',
+        subOptions: [
+            { name: 'Subopción 1.1', href: '#subopcion1.1' },
+            { name: 'Subopción 1.2', href: '#subopcion1.2' },
+        ],
+    },
+    {
+        name: 'Opción 2',
+        href: '#opcion2',
+        subOptions: [
+            { name: 'Subopción 2.1', href: '#subopcion2.1' },
+            { name: 'Subopción 2.2', href: '#subopcion2.2' },
+        ],
+    },
+    {
+        name: 'Opción 3',
+        href: '#opcion3',
+        subOptions: [],
+    },
+];
 const Layout = ({ children }) => {
-    return (
-        <div className="flex h-screen bg-gray-100">
-            {/* Barra Lateral */}
-            <aside className="w-64 bg-blue-700 text-white flex-shrink-0">
-                <div className="p-4 text-xl font-semibold">Calificación de Tesis</div>
-                <nav>
-                    <ul>
-                        <li className="p-4 hover:bg-blue-800">
-                            <a href="#calificaciones">Calificaciones</a>
-                        </li>
-                        <li className="p-4 hover:bg-blue-800">
-                            <a href="#resultados">Resultados</a>
-                        </li>
-                        <li className="p-4 hover:bg-blue-800">
-                            <a href="#configuracion">Configuración</a>
-                        </li>
-                    </ul>
-                </nav>
-            </aside>
+    // Lee el estado inicial de localStorage
+    const [isSidebarVisible, setSidebarVisible] = useState(() => {
+        const savedState = localStorage.getItem('isSidebarVisible');
+        return savedState !== null ? JSON.parse(savedState) : true;
+    });
+    const [openMenuIndex, setOpenMenuIndex] = useState(null);
+    const [nombreUsuario, setNombreUsuario] = useState('Usuario');
 
-            {/* Área de contenido */}
-            <div className="flex-1 flex flex-col">
-                {/* Barra superior */}
-                <header className="bg-white shadow p-4 flex justify-between items-center">
-                    <div className="text-lg font-semibold">Sistema de Calificación</div>
-                    <div className="flex items-center space-x-4">
-                        <span>Usuario</span>
-                        <img
-                            src="https://via.placeholder.com/32"
-                            alt="avatar"
-                            className="w-8 h-8 rounded-full"
-                        />
+    useEffect(() => {
+        // Obtiene el nombre del usuario desde localStorage
+        const userInfo = localStorage.getItem('userInfo');
+        if (userInfo) {
+            const userInfoParsed = JSON.parse(userInfo);
+            const nombre = userInfoParsed[0].nombre;
+            const apellido = userInfoParsed[0].apellido;
+
+            // Extraer solo el primer nombre y el primer apellido
+            const primerNombre = nombre.split(' ')[0];  // Toma la primera palabra antes del espacio
+            const primerApellido = apellido.split(' ')[0];  // Toma la primera palabra antes del espacio
+
+            // Solo el primer nombre y el primer apellido
+            setNombreUsuario(`${primerNombre} ${primerApellido}`);  // Pasa los valores al estado
+        }
+    }, []);
+
+
+    const toggleSidebar = () => {
+        setSidebarVisible((prevVisible) => {
+            const newVisible = !prevVisible;
+            localStorage.setItem('isSidebarVisible', JSON.stringify(newVisible));
+            return newVisible;
+        });
+    };
+
+    // Función para manejar el clic y alternar la visibilidad
+    const toggleSubOptions = (index) => {
+        setOpenMenuIndex(openMenuIndex === index ? null : index);
+    };
+
+    return (
+        <div className="min-h-screen flex flex-col bg-[#015198] text-gray-800">
+            {/* Barra superior */}
+            <header className="bg-gray-100 text-gray-800 p-2 flex justify-between items-center shadow-lg z-50">
+                <div className="flex items-center space-x-4">
+                    <button
+                        onClick={toggleSidebar}
+                        className="p-2 rounded-full hover:bg-[#015198] hover:text-white transition-all"
+                    >
+                        {isSidebarVisible ? (
+                            <XMarkIcon className="h-6 w-6" />
+                        ) : (
+                            <Bars3Icon className="h-6 w-6" />
+                        )}
+                    </button>
+                    <img src={logo} alt="Logo de Portocomercio" className="h-14 w-auto" />
+                </div>
+                <div className="flex items-center space-x-4">
+                    <span className="font-semibold text-lg">{nombreUsuario}</span>
+                    <div className="rounded-full w-10 h-10 bg-blue-500 flex justify-center items-center text-gray-800 font-semibold">
+                        {/* mostrar solo al primer letra del nombre */}
+                        {nombreUsuario.slice(0, 1)}
                     </div>
-                </header>
+                </div>
+            </header>
+
+            {/* Contenedor principal que incluye la barra lateral y el contenido */}
+            <div
+                className={`flex flex-grow overflow-hidden transition-all duration-300 ease-in-out`}
+            >
+                {/* Barra lateral */}
+                <aside
+                    className={`bg-[#015198] text-white p-6 space-y-6 w-64 h-full fixed transition-transform duration-300 ease-in-out ${isSidebarVisible ? 'translate-x-0 opacity-100' : '-translate-x-full opacity-0'
+                        }`}
+                >
+                    <nav className="space-y-4">
+                        {menuData.map((item, index) => (
+                            <div key={index}>
+                                {/* Opción principal con el ícono dentro del <a> */}
+                                <a
+                                    href={item.href}
+                                    className="flex items-center p-2 hover:bg-blue-600 rounded-md transition-colors flex-1 justify-between"
+                                    onClick={() => toggleSubOptions(index)} // Manejar clic para expandir/contraer
+                                >
+                                    {item.name}
+
+                                    {/* Indicador de subopciones */}
+                                    {item.subOptions.length > 0 && (
+                                        <span className="ml-2">
+                                            {openMenuIndex === index ? (
+                                                <ChevronUpIcon className="h-5 w-5 text-white" />
+                                            ) : (
+                                                <ChevronDownIcon className="h-5 w-5 text-white" />
+                                            )}
+                                        </span>
+                                    )}
+                                </a>
+
+                                {/* Subopciones (solo visibles si 'openMenuIndex' coincide con el índice actual) */}
+                                {item.subOptions.length > 0 && (
+                                    <div
+                                        className={`ml-4 overflow-hidden transition-all duration-300 ease-in-out ${openMenuIndex === index ? 'max-h-[1000px]' : 'max-h-0'
+                                            }`}
+                                    >
+                                        <div className="space-y-2">
+                                            {item.subOptions.map((subItem, subIndex) => (
+                                                <a
+                                                    key={subIndex}
+                                                    href={subItem.href}
+                                                    className="block p-2 hover:bg-blue-500 rounded-md transition-colors"
+                                                >
+                                                    {subItem.name}
+                                                </a>
+                                            ))}
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+                        ))}
+                    </nav>
+                </aside>
 
                 {/* Contenido principal */}
-                <main className="flex-1 p-0 overflow-y-auto">{children}</main>
+                <main
+                    className={`flex-grow  bg-white shadow-inner transition-all duration-300 ease-in-out ${isSidebarVisible ? 'ml-64' : 'ml-0'
+                        }`}
+                >
+                    {children}
+                </main>
             </div>
         </div>
     );
 };
 
-// Definición de prop-types para Layout
 Layout.propTypes = {
     children: PropTypes.node.isRequired,
 };
