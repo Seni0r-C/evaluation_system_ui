d6894babfcd6f6465d816256aa4efb88cba5989c
Carlos Arteaga <carlosluis199924@hotmail.com>
2024-11-26 21:52:34 -0500

    elime unas cosas que no se usaban

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,7 +1,5 @@
 export const API_URL = "http://localhost:3000";
-// export const API_PUENTE = "http://localhost:3000";
 
-// export const subRuta = "/app";
 export const Rutaraiz = "/";
 
 export const VERSION = "0.0.1";
