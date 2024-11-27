b793977f406366bf7a90986e73e47cc1650ed807
Carlos Arteaga <carlosluis199924@hotmail.com>
2024-11-26 21:56:56 -0500

    correji el nombre que yo habia puesto mal

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
 export const API_URL = "http://localhost:3000";
 
-export const RutaRaiz = "/";
+export const RutaRaiz = "/app";
 
 export const VERSION = "0.0.1";
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { Rutaraiz } from './src/utils/constants'
+import { RutaRaiz } from './src/utils/constants'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: Rutaraiz, // Cambia "subcarpeta" al nombre de tu carpeta
+  base: RutaRaiz,
 })
