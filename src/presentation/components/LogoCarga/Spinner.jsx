6c2648c4f3b404a122094d3491e5f504e32a3427
Seni0r-C <carlosluis199924@hotmail.com>
2024-12-15 19:09:12 -0500

    ahi esta el spinner definitivo

--- src/presentation/components/LogoCarga/Spinner.jsx
+++ src/presentation/components/LogoCarga/Spinner.jsx
@@ -1,11 +1,23 @@
 import './Spinner.css';
 
 const Spinner = () => {
-    return (
-        <div className="spinner">
-            <div className="circle"></div>
-        </div>
-    );
+  return (
+    <div className="loader">
+      <div className="loading-spinner">
+        <svg className="loading-spinner__circle-svg" viewBox="25 25 50 50">
+          <circle 
+            className="loading-spinner__circle-stroke" 
+            cx="50" 
+            cy="50" 
+            r="20" 
+            fill="none" 
+            strokeWidth="2" 
+            strokeMiterlimit="10" 
+          />
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export default Spinner;
