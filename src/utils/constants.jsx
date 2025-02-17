d21601810e8653c2a76f41852e76302dfba59ee9
Jostin Rodriguez <jarzta99@gmail.com>
2025-02-17 12:11:12 -0500

    Ajustes para que funcione el .env

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,4 @@
-export const API_URL = process.env.API_URL??"http://localhost:3000";
-// export const API_URL =  "http://172.29.33.226:3000";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export const RutaRaiz = "/";
 
