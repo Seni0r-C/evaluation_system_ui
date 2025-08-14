ea12984577855631c255d1a85c89729478249cb0
Carlos Arteaga <carlosluis199924@hotmail.com>
2025-08-13 20:16:29 -0500

    Elimina la configuración de la ruta base y los hosts permitidos en el archivo de configuración de Vite.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-// import { RutaRaiz } from './src/utils/constants'
+
+//Ruta base para producción
+// const baseRoute = "/gestion-titulacion"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/gestion-titulacion",
-  server: {
-    allowedHosts: ["justusas.com"]
-  }
 })  
