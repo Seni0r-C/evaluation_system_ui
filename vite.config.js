125c4fec460b5e41f3f4a19748cae0d792dd5a8f
Seni0r-C <carlosluis199924@hotmail.com>
2025-08-18 11:40:18 -0500

    Corrige la ruta base en .env.example y vite.config.js para incluir la barra final.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 //Ruta base para producción
-// const baseRoute = "/gestion-titulacion"
+// const baseRoute = "/gestion-titulacion/"
 
 // https://vite.dev/config/
 export default defineConfig({
